230d72b8c8b184c0595f65c197748bfff5c15b18
574515 <574515c0d1ng@gmail.com>
2020-06-03 20:03:55 +0200

    Prepravak 1. zadatka - pokušaj 3

--- src/edunova/Zadatak01.java
+++ src/edunova/Zadatak01.java
@@ -10,21 +10,17 @@ public class Zadatak01 {
 		int br1 = Integer.parseInt(JOptionPane.showInputDialog("Unesite prvi broj"));
 		int br2 = Integer.parseInt(JOptionPane.showInputDialog("Unesite drugi broj"));
 		int br3 = Integer.parseInt(JOptionPane.showInputDialog("Unesite treci broj"));
-		int jednak_Broj;
-		
-		if(br1 > br2 && br1 > br3) {
+			
+		if(br1 >= br2 && br1 >= br3) {
 			System.out.println(br1);
 		}
-		else if(br2 > br1 && br2 > br3) {
+		else if(br2 >= br1 && br2 >= br3) {
 			System.out.println(br2);
 		}
-		else if(br3 > br1 && br3 > br2){
+		else if(br3 >= br1 && br3 >= br2){
 			System.out.println(br3);
 		}
-		else {
-			jednak_Broj = br1 & br2 & br3;
-			System.out.println(jednak_Broj);
-		}
+		
 	}
 	
 }
