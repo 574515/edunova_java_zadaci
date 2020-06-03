3c366f515312c20e20213617362ad14d292498af
574515 <574515c0d1ng@gmail.com>
2020-06-03 19:37:54 +0200

    Prvi zadatak - gotov

---
+++ src/edunova/Zadatak01.java
@@ -0,0 +1,25 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Zadatak01 {
+
+	// Korisnik unosi 3 broja, ispisuje se najveci
+	public static void main(String[] args) {
+		 
+		int br1 = Integer.parseInt(JOptionPane.showInputDialog("Unesite prvi broj"));
+		int br2 = Integer.parseInt(JOptionPane.showInputDialog("Unesite drugi broj"));
+		int br3 = Integer.parseInt(JOptionPane.showInputDialog("Unesite treci broj"));
+		
+		if(br1 > br2 && br1 > br3) {
+			System.out.println(br1);
+		}
+		else if(br2 > br1 && br2 > br3) {
+			System.out.println(br2);
+		}
+		else {
+			System.out.println(br3);
+		}
+	}
+	
+}
